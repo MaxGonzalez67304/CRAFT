199e89a9cb1f0f7be50303eeca2dbf6747799e1d
Max Gonzalez <castor67304@hotmail.com>
2022-06-19 18:48:04 -0500

    Implementacion de alertas especiales

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { DataBaseService } from 'src/app/services/database.service';
 import { GlobalService } from 'src/app/global.service';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -12,7 +13,7 @@ import { GlobalService } from 'src/app/global.service';
 })
 export class LoginComponent implements OnInit {
 
-  validarSpeak: GlobalService; 
+  validarSpeak: GlobalService;
   mensaje: any;
   oracion: any;
 
@@ -25,7 +26,7 @@ export class LoginComponent implements OnInit {
 
   }
 
-  constructor(private authService: AuthService, private router: Router, global: GlobalService) { 
+  constructor(private authService: AuthService, private router: Router, global: GlobalService) {
     if ('speechSynthesis' in window) {
       this.mensaje = new SpeechSynthesisUtterance();
     } else {
@@ -35,29 +36,48 @@ export class LoginComponent implements OnInit {
     console.log(this.validarSpeak);
   }
 
-  playSpeak(texto2:string) {  
+  playSpeak(texto2: string) {
     this.oracion = document.getElementById(texto2)!.innerHTML;
-    this.mensaje.text= this.oracion; 
-    if(speechSynthesis.paused){
+    this.mensaje.text = this.oracion;
+    if (speechSynthesis.paused) {
       speechSynthesis.resume();
-    }else{
+    } else {
       speechSynthesis.cancel();
       speechSynthesis.speak(this.mensaje);
     }
   }
 
-  stopSpeak(){
+  stopSpeak() {
     speechSynthesis.pause();
   }
-  
+
+  alertaEXITO() {
+    Swal.fire({
+      icon: 'success',
+      title: 'EXITO',
+      text: 'INGRESO EXITOSO',
+    })
+  }
+
+  alertaFAIL() {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR',
+      text: 'DATOS INCORRECTOS',
+    })
+  }
+
   Ingresar() {
     const { email, password } = this.usuario;
     this.authService.login(email, password).then(user => {
       console.log("Bienvenido ", user);
       if (!user) {
-        alert("Datos incorrectos, si no tenes cuenta registrate!");
+        this.alertaFAIL();
         return;
+      } else {
+        this.alertaEXITO();
       };
+      
       this.router.navigate(['/home']);
     }).catch(err => {
       console.log(err)
